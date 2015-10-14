b15e359134ffc8c2b8f2585584807cd030a8ed8c
toye4eva01 <toye4eva01@yahoo.com>
2015-10-14 13:21:03 +0100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -28,7 +28,21 @@ var mainView = myApp.addView('.view-main', {
 
 
 $(document).on('pageInit', function (e) {
-	
+	document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        e.preventDefault(); 
+    navigator.notification.confirm("Are you sure want to exit from App?", onConfirmExit, "Confirmation", "Yes,No");
+    }, false );
+}
+ 
+function onConfirmExit(button) {
+    if(button==2){ //If User select a No, then return back;
+        return;
+    }else{
+        navigator.app.exitApp(); // If user select a Yes, quit from the app.
+    }
+}
 	
 	
 	/*$('.page-content').load('offerslides.php', function() {*/
